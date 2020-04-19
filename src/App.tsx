223417f946ce4474cee9329c6f2b536d2d88a1f5
Roch D'Amour <roch.damour@gmail.com>
2020-04-19 00:14:20 -0400

    Router pass arg to catalog, grid css

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import tw from "tailwind.macro";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { CatalogPage } from "./Components/Pages/Catalog";
 
-const Background = styled.div`
-  ${tw`bg-green-100 h-screen`}
-`;
-
 function App() {
   return (
     <Background>
@@ -18,19 +14,26 @@ function App() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/catalog">Catalog</Link>
+              <Link to="/boards/Random/catalog">Catalog</Link>
             </li>
           </ul>
         </nav>
         <Switch>
-          <Route path="/catalog">
-            <h2>Catalog page</h2>
-            <CatalogPage message={"This is my message"} />
-          </Route>
+          <Route
+            exact
+            path="/boards/:board/catalog"
+            render={props => (
+              <CatalogPage board={props.match.params.board || ""} />
+            )}
+          />
         </Switch>
       </Router>
     </Background>
   );
 }
+
+const Background = styled.div`
+  ${tw`bg-green-100 min-h-screen max-w-screen`}
+`;
 
 export default App;
--- src/Components/Pages/Catalog.tsx
+++ src/Components/Pages/Catalog.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import tw from "tailwind.macro";
 import { range } from "lodash";
+import tempPost from "../../ressources/tempPost.json"; //TODO: Query data from backend
 
-interface Props {
-  message: string;
+interface CatalogProps {
+  board: string;
 }
 
 interface Post {
@@ -14,38 +15,44 @@ interface Post {
   uri: string;
 }
 
-export const CatalogPage = (props: Props) => {
-  const examplePost: Post = {
-    id: 0,
-    name: "Shill my new client",
-    content: "its too good",
-    uri: "https://source.unsplash.com/rcolors-andom"
-  };
-
+export const CatalogPage: React.FC<CatalogProps> = (props: CatalogProps) => {
   const posts: [Post?] = [];
 
   range(50).forEach((id: number) => {
     posts.push({
-      ...examplePost,
+      ...tempPost,
       id
     });
   });
 
   return (
-    <CatalogContainer>
-      {posts.map(post => (
-        <PostBlock>
-          <h4>{post?.name}</h4>
-        </PostBlock>
-      ))}
-    </CatalogContainer>
+    <>
+      <BoardTitle>
+        <h1>{props.board}</h1>
+      </BoardTitle>
+      <CatalogContainer>
+        {posts.map(post => (
+          <PostBlock>
+            <h4>{post?.name}</h4>
+          </PostBlock>
+        ))}
+      </CatalogContainer>
+    </>
   );
 };
 
+const BoardTitle = styled.div`
+  ${tw`text-center text-6xl`}
+`;
+
 const CatalogContainer = styled.div`
-  ${tw`block`}
+  grid-row-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  grid-auto-rows: min-content;
+  display: grid;
+  justify-items: center;
 `;
 
 const PostBlock = styled.div`
-  ${tw`border-solid border-2 border-black bg-green-200 m-4 p-2`}
+  ${tw`flex border-solid border border-black bg-green-200 m-4 p-2`}
 `;
